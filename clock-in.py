debbd6baff30956109191082e525bf4ad5cbbe65
Ais0n <aisooon@yahoo.com>
2022-05-07 21:04:10 +0800

    new capt

--- clock-in.py
+++ clock-in.py
@@ -8,6 +8,7 @@ import re
 import datetime
 import time
 import sys
+import ddddocr
 
 
 class DaKa(object):
@@ -67,6 +68,31 @@ class DaKa(object):
         today = datetime.date.today()
         return "%4d%02d%02d" % (today.year, today.month, today.day)
 
+    def getCap(self):
+        captcha_url = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
+        ocr = ddddocr.DdddOcr()
+
+        # 设置 cookie
+        cookie_dict = {'eai-sess': 'xxxxxxxxxxxxxxxxxxxxxxxxx'}
+        self.sess.cookies = requests.cookies.cookiejar_from_dict(cookie_dict)
+        headers = {
+            'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cookies': "UUkey=d1da8dbba9615d22aada045b55dfdf80; _pm0=LqYPI/LKC2ywk/py3jWzWzgKbG7oemmzvah6t5ZyI7E=; _ga_MWF1PQPJ1G=GS1.1.1645427272.2.1.1645427361.0; _ga=GA1.3.982320635.1630220073; BSFIT_pi620=; BSFIT_mjvys=; BSFIT_h9oq/=; BSFIT_16jgo=; BSFIT_i1yu/=; BSFIT_qpkBr=; BSFIT_jAhq6=; BSFIT_nl5/D=+p9d+/kd+S+Prpk3+n,+/n0F/Ew+/Eerj; Hm_lvt_35da6f287722b1ee93d185de460f8ba2=1650181662,1650372014,1650379670,1650952508; Hm_lvt_fe30bbc1ee45421ec1679d1b8d8f8453=1651135879; BSFIT_hojBi=; sudy_ck=1217FE1E8FB488B79051BF9DBFE85E61998399B74C571D73ADB484B09400A25FAD03709E2AABA05FCDF0FFC3E93A07A9551F55B2C4400C50F2114ACC7C40DBBF434ECB043354687DDB1D364FDA9E435D; _csrf=S8mwplVi9KWoF2WQ0TlCeIyKFfa6YWb1wPo5qvZHAEw=; _pv0=k8SP5WS7AK/JtmEi0Wzprl5VZnAAbevVn5TInLzuTpoDvKoE3pbwOup+1BI0nxS1tPoSrc4btq2Hv7ZRx1YV8e+GNneORD53IOBXKiUf4JAcUfdnWEU2P1kwWfde8QOUCJPXC2abKKX+h1uyGBsQMqbtpM04l8+TR78kmYv0kmKAd8jy2VmnUl0hfpWM3WFCTbmMcUY+HdLMfLR5RAPisqAmC8KZOeM8/xsgOlFYtCj31GXoegHLa7LgdLbkysago/6e/Aijljx/ZsWP4Wy/GLufm4KGSH2PfT9RwTsWCdjcQXVyQyyu8WzP8J+624DpPvRLGqAIwK8MXjAZWFmz3q/gZqxrbhB0y0vkf+LAcYNc6gkCxZrVOZGRzbsUDOTsSolN5sWaDfSCogf1lg62thlBNZ7Fg2mDYcBL3rxnOtE=; _pf0=kzzJAap65cCVqaKJKc/JQr7vQSFDbUca0YFbdw6WYJo=; _pc0=ZhowFVGAMLPBiozd6AcL3whIqXFBtgHBjW9GakLhV6tVI4aQ+uwK5i72wHH+/vXj; iPlanetDirectoryPro=MXHuWmX2b6UqtZdPrkbjfCXRxSNhwHkPH/fGHYi4QinZWai2DW9ppE7f2OLLlhb9xPRQnLp10JQ6Mx2gG9jQ7p5ES/bB/lHTOKV3/fzAUOUtFpSSVGC+g7+XaP4mVLGIfU0P0rnqh5ZuRhwC4Wc9UvGbH/8ILb55acTwOusgJWOCgMpeJq3G4NNQyOKLvBdc5SnbBRtjKPASg5GptucfrklCyQcYC4HXGfogZtW4VjttFv/oM5cdUbMp7l74/9b2PEC51X2BJJO7i+cMb1JgQLHOMfDAPpDtDGYM6qncFsHMugR9LZs2MeUkUc4P2rD6wSFIEsrCz8N533Iygijzq4T6UScrD6ohtFmYnvmnBl0=; eai-sess=5cnlcsbgedufqs1fdf9ap01g52; Hm_lvt_48b682d4885d22a90111e46b972e3268=1649494238,1650423551,1651926860; Hm_lpvt_48b682d4885d22a90111e46b972e3268=1651927709",
+            'referer': 'https://healthreport.zju.edu.cn/ncov/wap/default/index',
+            'sec-ch-ua-platform': "Windows",
+            'sec-fetch-dest': 'image',
+            'sec-fetch-mode': "no-cors",
+            'sec-fetch-site': 'same-origin',
+        }
+
+        resp = self.sess.get(url=captcha_url, headers=headers)
+        captcha = ocr.classification(resp.content)
+        print(captcha)
+        return captcha
+
     def get_info(self, html=None):
         """Get hitcard info, which is the old info with updated new time."""
         if not html:
@@ -109,6 +135,7 @@ class DaKa(object):
         new_info['jcqzrq'] = ""
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
+        new_info['verifyCode'] = self.getCap()
         self.info = new_info
         return new_info
 
